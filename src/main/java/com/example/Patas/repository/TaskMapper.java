ca7d208972d1ece21de4ca4bfd6506023c9723ad
yokosuka_susumu <yokosuka.susumu@alhinc.jp>
2025-06-25 13:36:30 +0900

    絞り込みできた

--- src/main/java/com/example/Patas/repository/TaskMapper.java
+++ src/main/java/com/example/Patas/repository/TaskMapper.java
@@ -1,7 +1,7 @@
 package com.example.Patas.repository;
 
 import com.example.Patas.repository.entity.Task;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
@@ -13,6 +13,7 @@ public interface TaskMapper {
     //全件取得
     List<Task> findAll();
 
+    //一件投稿取得
     Task findById(Integer id);
 
     //投稿削除
@@ -27,4 +28,16 @@ public interface TaskMapper {
     //ステータス変更
     void update(@Param("id") Integer id, @Param("status") Integer status,
               @Param("updatedDate") Date updatedDate);
+
+    //日付 絞り込み
+    List<Task> searchDate(Date start, Date end);
+
+    //日付 ステータス 絞り込み
+    List<Task> searchStatus(Date start, Date end, int status);
+
+    //日付 内容 絞り込み
+    List<Task> searchContent(Date start, Date end, String content);
+
+    //日付 内容 ステータス 絞り込み
+    List<Task> searchExactMatch(Date start, Date end, String content, int status);
 }
--- src/main/java/com/example/Patas/service/TaskService.java
+++ src/main/java/com/example/Patas/service/TaskService.java
@@ -25,6 +25,7 @@ public class TaskService {
 
     //１．タスク情報取得処理
     public List<TaskForm> findAllTask(){
+
         List<Task> results = taskMapper.findAll();
         List<TaskForm> tasks = setTaskForm(results);
         return tasks;
@@ -41,13 +42,17 @@ public class TaskService {
         List<Task> results = null;
         if(StringUtils.isBlank(taskForm.getContent())
                 && ((taskForm.getStatus() == null) || taskForm.getStatus() == 0 || taskForm.getStatus() == 5)) {
-            results = taskRepository.findTop9ByLimitDateBetweenOrderByLimitDateAsc(startDate, endDate);
+            //results = taskRepository.findTop9ByLimitDateBetweenOrderByLimitDateAsc(startDate, endDate);
+            results = taskMapper.searchDate(startDate,endDate);
         } else if(StringUtils.isBlank(taskForm.getContent())) {
-            results = taskRepository.findTop9ByLimitDateBetweenAndStatusOrderByLimitDateAsc(startDate, endDate, taskForm.getStatus());
+            //results = taskRepository.findTop9ByLimitDateBetweenAndStatusOrderByLimitDateAsc(startDate, endDate, taskForm.getStatus());
+            results = taskMapper.searchStatus(startDate, endDate, taskForm.getStatus());
         } else if ((taskForm.getStatus() == null || taskForm.getStatus() ==0 || taskForm.getStatus() == 5)){
-            results = taskRepository.findTop9ByLimitDateBetweenAndContentOrderByLimitDateAsc(startDate, endDate, taskForm.getContent());
+            //results = taskRepository.findTop9ByLimitDateBetweenAndContentOrderByLimitDateAsc(startDate, endDate, taskForm.getContent());
+            results = taskMapper.searchContent(startDate, endDate, taskForm.getContent());
         } else {
-            results = taskRepository.findTop9ByLimitDateBetweenAndContentAndStatusOrderByLimitDateAsc(startDate, endDate, taskForm.getContent(), taskForm.getStatus());
+            //results = taskRepository.findTop9ByLimitDateBetweenAndContentAndStatusOrderByLimitDateAsc(startDate, endDate, taskForm.getContent(), taskForm.getStatus());
+            results = taskMapper.searchExactMatch(startDate, endDate, taskForm.getContent(), taskForm.getStatus());
         }
         List<TaskForm> tasks = setTaskForm(results);
         return tasks;
